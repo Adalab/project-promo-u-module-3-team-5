8cfbfce69561fc230465485f7711e290b302280b
diana_marmol <dianamarmolus@gmail.com>
2023-10-19 19:30:31 +0200

    Small scss changes, fix top

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,8 +2,8 @@ import logoNuestro from "../../images/logo-nuestro.png";
 import { Link } from "react-router-dom";
 const Landing = () => {
   return (
-    <div>
-        <Link to="/create-projects">Crear Proyectos Cohete <img src={logoNuestro} alt="cohete"></img> </Link>
+    <div className="landing">
+        <Link to="/create-projects" className="btn">Crear Proyectos Cohete <img src={logoNuestro} alt="cohete" className="logo"></img> </Link>
     </div>
   )
 }
